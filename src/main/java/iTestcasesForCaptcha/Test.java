4431745e43004192691c89a7512467cdea3da545
Madhuchandha-Engineer <madhuchandhap2001@gmail.com>
2025-05-02 15:40:01 +0530

    Initial commit

---
+++ src/main/java/iTestcasesForCaptcha/Test.java
@@ -0,0 +1,13 @@
+package iTestcasesForCaptcha;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String a = "1. Question id [ 10651 ]";
+		
+		System.out.println(a.contains(" ["));
+
+	}
+
+}
